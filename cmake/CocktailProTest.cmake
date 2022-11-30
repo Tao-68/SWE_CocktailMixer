a3d3b64dba37e2acd5e0fd06d38586af8089cf48
Alexander Bohn <alexander.a.bohn@stud.h-da.de>
2022-11-30 15:29:10 +0100

    add automatically test file in CocktailPrTest.cmake

--- cmake/CocktailProTest.cmake
+++ cmake/CocktailProTest.cmake
@@ -1,7 +1,7 @@
 # Add all test files including the file holding the test main function
 add_executable(CocktailProTest
         ../src/test/RecipeBook_Test.cpp
-        ../src/test/AvailableIngredients_Test.cpp)
+        ../src/test/AvailableIngredients_Test.cpp ../src/test/DeviceManager_Test.cpp ../src/test/Dispenser_Test.cpp ../src/test/Drainer_Test.cpp ../src/test/InternalDevice_Test.cpp)
 
 # Link CocktailProTest against CocktailProSource
 linkCocktailProSource(CocktailProTest)
